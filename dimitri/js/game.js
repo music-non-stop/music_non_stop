3e27b966aacb585db1c2b2b805a086042890f101
dimitri-edel <dimitri.edel@gmx.net>
2024-09-19 20:19:25 +0200

    Overide folder dimitri and add new version of game.js

--- dimitri/js/game.js
+++ dimitri/js/game.js
@@ -1,32 +1,61 @@
+class GameCard {
+    constructor(index, trackIndex) {
+        // use index for the card id
+        this.index = index;
+        this.trackIndex = trackIndex;
+    }
+    render() {
+        return `
+            <div class="flip-card">
+                <div id="card-${this.index}" class="flip-card-inner" onclick="card_clicked(${this.index})">
+                    <div class="flip-card-front">
+                        <h4>Track ${this.trackIndex}</h4>
+                    </div>
+                    <div class="flip-card-back">
+                        <p>Listen and pick another card</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
 class Game {
-    constructor(cards) {
-        this.cards = cards;  // Array of card objects
-        this.selectedCard = null;  // The first card selected
-        this.matchedPairs = 0;  // Track matched pairs
+    constructor(cards, mp3player) {
+        this.cards = cards;
+        this.mp3player = mp3player;
+        this.firstCard = null;
+        this.secondCard = null;
+        this.score = 0;        
     }
 
-    pickCard(cardIndex) {
-        const card = this.cards[cardIndex];
+    pickCard = (n) => {
+        // Play the song associated with the card
+        this.mp3player.play(this.cards[n].trackIndex);
 
-        if (this.selectedCard === null) {
-            // First card picked
-            this.selectedCard = card;
-            return false;  // No match yet
-        } else {
-            // Second card picked, check for match
-            const isMatch = this.selectedCard.track === card.track;
-            if (isMatch) {
-                this.matchedPairs += 1;
-                this.selectedCard = null;  // Reset for the next turn
-                return true;  // Match found
-            } else {
-                this.selectedCard = null;  // No match, reset selection
-                return false;  // No match
-            }
+        if(this.firstCard == null) {
+            this.firstCard = this.cards[n];
+        }else{
+            this.secondCard = this.cards[n];
         }
+
+        return this.isAMatch(n);
     }
 
-    isGameOver() {
-        return this.matchedPairs === this.cards.length / 2;  // All pairs matched
+    
+    // See if both cards have the same index and are not the same card
+    isAMatch = (n) => {
+        // If both cards are null, it means that only one has been selected
+        if (this.firstCard == null || this.secondCard == null) {
+            return false;
+        }
+
+        if (this.firstCard.trackIndex == this.secondCard.trackIndex && this.firstCard.index != n) {
+            this.firstCard = null;
+            this.secondCard = null;
+            this.score++;
+            return true;
+        }
+        return false;
     }
 }
